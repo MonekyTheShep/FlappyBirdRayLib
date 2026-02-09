4b43a0c236abd9a0de5ab41d87a340ea420df3f9
Moneky <237399958+MonekyTheShep@users.noreply.github.com>
2026-02-09 18:10:28 +0000

    Initial commit

---
+++ sources/main.c
@@ -0,0 +1,24 @@
+#include "raylib.h"
+
+#define SCREEN_WIDTH (800)
+#define SCREEN_HEIGHT (600)
+
+#define WINDOW_TITLE "Flappy Bird"
+
+int main(void)
+{
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
