0a282387e70999074cee165a5b4ad6436f67f651
Moneky <237399958+MonekyTheShep@users.noreply.github.com>
2026-02-11 00:24:39 +0000

    Ill organise the game state later

--- src/states/gamestate.c
+++ src/states/gamestate.c
@@ -4,43 +4,42 @@
 #include "pipe.h"
 #include "bird.h"
 
-Bird bird;
+Bird bird = {
+    .jumpVel = -250.0f,
+    .gravVel = 200.0f
+};
+
 Pipe pipe;
 
 void initializeGame(void) {
-    Pipe pipe = {
-        .pipeBottom = LoadTexture(ASSETS_PATH"/pipe_bottom.png"),
-        .pipeTop = LoadTexture(ASSETS_PATH"/pipe_top.png"),
-        .pipeChunk = LoadTexture(ASSETS_PATH"/pipe_chunk.png"),
-        .position = (Vector2) {GetScreenWidth() / 2, GetScreenHeight() / 2},
-        .velocity = (Vector2) {0.0f, 0.0f}
-    };
 
-    Bird bird = {
-        .sprite = LoadTexture(ASSETS_PATH"/flappy_bird.png"),
-        .jumpVel = -250.0f,
-        .gravVel = 200.0f,
-        .position = (Vector2) {50, GetScreenWidth() / 2},
-        .velocity = (Vector2) {0.0f, 0.0f}
-    };
+    // Pipe declaring
+    pipe.pipeBottom = LoadTexture(ASSETS_PATH"/pipe_bottom.png");
+    pipe.pipeTop = LoadTexture(ASSETS_PATH"/pipe_top.png");
+    pipe.pipeChunk = LoadTexture(ASSETS_PATH"/pipe_chunk.png");
+    pipe.position = (Vector2) {(float) GetScreenWidth() / 2 + 40, (float) GetScreenHeight() / 2};
+    pipe.velocity = (Vector2) {0.0f, 0.0f};
 
+    // Bird declaring
     const float factor = 0.2f;
+    bird.sprite = LoadTexture(ASSETS_PATH"/flappy_bird.png");
     bird.src = (Rectangle) {0,0, (float) bird.sprite.width, (float) bird.sprite.height};
     bird.hitBox = (Rectangle) {bird.position.x, bird.position.y, (float) bird.sprite.width * factor, (float) bird.sprite.height * factor};
+    bird.position = (Vector2) {50, (float) GetScreenHeight() / 2};
 }
 
 void updateGameMenu(GameInfo *gameInfo, MenuStates *menuState) {
-    const float deltaTime = GetFrameTime();
+        const float deltaTime = GetFrameTime();
 
         // Use this later
-        const int touchingFloor = (bird.hitBox.y + bird.hitBox.height >= GetScreenHeight());
+        const int touchingFloor = (bird.hitBox.y + bird.hitBox.height >= (float) GetScreenHeight());
         const int touchingCeiling = (bird.hitBox.y <= 0);
 
         // Floor and Ceiling detection
         if (touchingFloor) {
-            // In the real game i will just make the bird gameover.
-            // Prevent bird going under ground
-            bird.position.y = GetScreenHeight() - bird.hitBox.height;
+            // In the real game I will just make the bird gameover.
+            // Prevent bird going underground
+            bird.position.y = (float) GetScreenHeight() - bird.hitBox.height;
             // if the bird is going downwards then set velocity to 0
             if (bird.velocity.y > 0) bird.velocity.y = 0;
         }
@@ -78,7 +77,7 @@ void updateGameMenu(GameInfo *gameInfo, MenuStates *menuState) {
             ClearBackground(RAYWHITE);
 
             DrawTextureEx(pipe.pipeBottom, (Vector2) {0,(float) GetScreenHeight() / 2}, 0.0f, 0.5f,  WHITE);
-            DrawTextureEx(pipe.pipeChunk, (Vector2) {0,(float) GetScreenHeight() - pipe.pipeBottom.height / 2}, 0.0f, 0.5f,  WHITE);
+            DrawTextureEx(pipe.pipeChunk, (Vector2) {0,(float) GetScreenHeight() - (float) pipe.pipeBottom.height / 2}, 0.0f, 0.5f,  WHITE);
 
             if (deltaTime != 0) {
                 DrawText(TextFormat("CURRENT FPS: %i", (int)(1.0f/deltaTime)),  0, 0, 20, GREEN);
