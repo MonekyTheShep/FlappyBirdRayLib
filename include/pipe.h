588eed6314df06e65f959c2a6e4cd1fd061b388f
Moneky <237399958+MonekyTheShep@users.noreply.github.com>
2026-02-12 19:16:56 +0000

    initialise scored

--- include/pipe.h
+++ include/pipe.h
@@ -15,9 +15,9 @@ typedef struct Pipe {
 } Pipe;
 
 void drawPipe(Pipe *pipe);
-void handlePipes(Pipe *pipe, Bird *bird, int *gameOver);
 void initializePipePool(Pipe *pipePool);
 void initializePipe(Pipe *pipe);
 Pipe *acquirePipe(Pipe *pipePool);
 void releasePipe(Pipe *pipe);
 void drawPipes(Pipe *pipe);
+void handlePipes(Pipe *pipe, Bird *bird, int *gameOver);
