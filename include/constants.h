8519db50d3510ebaa2ab96f02fba289889750472
Moneky <237399958+MonekyTheShep@users.noreply.github.com>
2026-02-27 11:53:24 +0000

    refactor (constants): increase gravity and jump velocity

--- include/constants.h
+++ include/constants.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define GRAVITY_VELOCITY (200.0f)
-#define JUMP_VELOCITY (-175.0f)
+#define GRAVITY_VELOCITY (980.0f)
+#define JUMP_VELOCITY (-350.0f)
 #define ROTATION_VELOCITY (-100.0f)
 #define FRICTION (0.1f)
 #define PIPE_SPEED (-100.0f)
