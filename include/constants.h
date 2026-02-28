fde59d084f87fb899c03e7f7669efa59874a7b6b
Moneky <237399958+MonekyTheShep@users.noreply.github.com>
2026-02-28 19:16:05 +0000

    Update constants.h

--- include/constants.h
+++ include/constants.h
@@ -1,9 +1,11 @@
 #pragma once
 
+// World
 #define GRAVITY_VELOCITY (980.0f)
 #define JUMP_VELOCITY (-350.0f)
-#define ROTATION_VELOCITY (-100.0f)
 #define FRICTION (0.1f)
+
+// Pipe
 #define PIPE_SPEED (-150.0f)
 #define POOL_SIZE 5
 #define PIPE_SPAWN_RATE 2.0f
