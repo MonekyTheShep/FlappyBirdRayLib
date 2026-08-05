035b47ce22c0e9d58964d518940f2f2feb995fba
Moneky <237399958+MonekyTheShep@users.noreply.github.com>
2026-08-05 14:18:37 +0100

    Update assets.h

--- include/assets.h
+++ include/assets.h
@@ -1,10 +1,21 @@
 #pragma once
 
-#include "constants.h"
 #include <raylib.h>
 
 //----------------------------------------------------------------------------------
 // Global Asset Variables
 //----------------------------------------------------------------------------------
+enum Sounds
+{
+    PLACEHOLDER_MUSIC_SOUND,
+    NUM_OF_SOUND
+};
+
+enum Musics
+{
+    PLACEHOLDER_MUSIC,
+    NUM_OF_MUSIC
+};
+
 extern Sound sounds[NUM_OF_SOUND];
 extern Music music[NUM_OF_MUSIC];
